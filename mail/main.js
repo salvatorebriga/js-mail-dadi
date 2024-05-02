e443829618ce0c53307ac72638e271bee12691a0
salvatorebriga <salvo.z2302@gmail.com>
2024-05-02 14:28:19 +0200

    added login function

--- mail/main.js
+++ mail/main.js
@@ -1,17 +1,45 @@
 "use strict"
 
+/*popolamento array*/
+
 const email = ['salvo.z2302@gmail.com', 'zluke507@gmail.com', 'mariorossi38@gmail.com', 'peppino@gmail.com', 'ajejebrazorf@biglietto.com'];
 
-function myLogin(){
+/*stampa array*/
 
+document.getElementById('scope').innerHTML = 'Email già presenti: ';
+
+for(let i = 0; i < email.length; i++){
+    if(i === email.length - 1){
+        document.getElementById('scope').innerHTML += email[i];
+    }else{
+        document.getElementById('scope').innerHTML += email[i] + ', ';
+    }
+}
+
+
+
+
+function myLogin(){
+    let verifyEmail = document.getElementById('email').value;
+
+    for(let i = 0; i < email.length; i++){
+        if(verifyEmail === email[i]){
+            console.log('email presente nel DB');
+            return email[i];
+        }
+    }
+    console.log('email non presente nel DB');
 }
 
 function myRegister(){
+
     let newEmail = document.getElementById('email').value;
 
     let i = email.length;
 
     email[i] = newEmail;
+
+    document.getElementById('scope').innerHTML += ', ' + email[i];
 
 }
 
