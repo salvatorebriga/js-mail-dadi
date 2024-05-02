f46e61c294d8ca4113c6caa462ae05ee00a5ce5b
salvatorebriga <salvo.z2302@gmail.com>
2024-05-02 13:58:27 +0200

    added registration and show button

--- mail/main.js
+++ mail/main.js
@@ -1,1 +1,22 @@
 "use strict"
+
+const email = ['salvo.z2302@gmail.com', 'zluke507@gmail.com', 'mariorossi38@gmail.com', 'peppino@gmail.com', 'ajejebrazorf@biglietto.com'];
+
+function myLogin(){
+
+}
+
+function myRegister(){
+    let newEmail = document.getElementById('email').value;
+
+    let i = email.length;
+
+    email[i] = newEmail;
+
+}
+
+function myShow(){
+    for(let i = 0; i < email.length; i++){
+        console.log(email[i]);
+    }
+}
